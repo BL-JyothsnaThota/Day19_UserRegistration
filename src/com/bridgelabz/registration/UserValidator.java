6c4636d01682c7bd0ac8f9f4aa78b318857a862c
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-05-12 17:19:17 +0530

    Validate Last name starts with Cap and has minimum 3 characters

--- src/com/bridgelabz/registration/UserValidator.java
+++ src/com/bridgelabz/registration/UserValidator.java
@@ -3,15 +3,30 @@ import java.util.regex.Pattern;
 
 public class UserValidator {
 
-    // UC1 - First Name Validation
-    public static boolean validateFirstName(String firstName) {
+    // Common method for Name Validation (Reusable)
+    public static boolean validateName(String name) {
         String regex = "^[A-Z][a-zA-Z]{2,}$";
-        return Pattern.matches(regex, firstName);
+        return Pattern.matches(regex, name);
+    }
+
+    // UC1
+    public static boolean validateFirstName(String firstName) {
+        return validateName(firstName);
+    }
+
+    // UC2
+    public static boolean validateLastName(String lastName) {
+        return validateName(lastName);
     }
 
     public static void main(String[] args) {
+
+        // UC1
         System.out.println(validateFirstName("John"));   // true
-        System.out.println(validateFirstName("jo"));     // false
-        System.out.println(validateFirstName("J"));      // false
+
+        // UC2
+        System.out.println(validateLastName("Doe"));     // true
+        System.out.println(validateLastName("doe"));     // false
+        System.out.println(validateLastName("Do"));      // false
     }
 }
